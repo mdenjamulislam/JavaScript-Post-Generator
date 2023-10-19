66d9058888013c789aba387877bbde36bac17ee5
Mehedi Robin <robinicage@gmail.com>
2023-10-20 00:31:45 +0600

    Solved the TypeError

--- js/script.js
+++ js/script.js
@@ -1,20 +1,16 @@
 import { generateTextAndImage } from "./utils.js";
 
+const postGenerateBtn = document.getElementById("generate-post");
 
-const postGenerateBtn = document.getElementById('generate-post');
+postGenerateBtn.addEventListener("click", () => {
+  let name = document.getElementById("name").value;
+  let favoriteActivity = document.getElementById("favorite-activity").value;
+  let favoritePlace = document.getElementById("favorite-place").value;
+  let temperature = 0.6;
+  // Reset Values
+  document.getElementById("name").value = "";
+  document.getElementById("favorite-activity").value = "";
+  document.getElementById("favorite-place").value = "";
+  generateTextAndImage(name, favoriteActivity, favoritePlace, temperature);
 
-postGenerateBtn.addEventListener('click', () => {
-    const name = document.getElementById('name').value;
-    const favoriteActivity = document.getElementById('favorite-activity').value;
-    const favoritePlace = document.getElementById('favorite-place').value; 
-    let temperature = 0.6;
-    // Reset Values
-    name.value = '';
-    favoriteActivity.value = '';
-    favoritePlace.value = '';
-    
-    generateTextAndImage(name, favoriteActivity, favoritePlace, temperature);
 });
-
-
-
